f4fda199c2d82db3f38a0fac64278f3bcad6108e
Eb-dur <malte.hammar01@gmail.com>
2024-12-10 16:18:10 +0100

    Container requirements should be finished, just need to do some qol functions as well

--- longlistw32.cc
+++ longlistw32.cc
@@ -13,76 +13,90 @@
 #include <algorithm>
 #include <iostream>
 #include <limits>
+#include <algorithm>
 
 template <typename T>
 class LongList{
-public:
+public: 
     using value_type = T;
     using reference = T&;
     using const_reference = const T&;
     using size_type = unsigned int;
-    /*
-    typename C::iterator 	an iterator type 	
-        C::iterator is a LegacyForwardIterator, and its value type is T.
-        C::iterator is convertible to C::const_iterator. 
+    using difference_type = std::ptrdiff_t;
 
-    typename C::const_iterator 	a constant iterator type 	C::const_iterator is a LegacyForwardIterator, and its value type is T.
-    typename C::difference_type 	a signed integer type 	C::difference_type is the same as the difference type of C::iterator and C::const_iterator. 
-    */
-    LongList() = default;
-    LongList(const LongList&);
-    LongList(const LongList&&);
-    LongList<T>& operator=(const LongList&);
-    LongList<T>& operator=(const LongList&&);
-    ~LongList();
-    void insert(T);
 private:
-    
     struct node
     {
         node* next{nullptr};
         T data{};
     };
 
-    class iterator{
-        // https://en.cppreference.com/w/cpp/named_req/ForwardIterator
-    public:
-        using iterator_category = std::forward_iterator_tag;
-        using value_type = T;
-        using difference_type = std::ptrdiff_t;
-        using pointer = T*;
-        using reference = T&;
-
-
-        iterator() = default;
-        iterator(const iterator& other) = default;
-        iterator& operator=(const iterator& other) = default;
-        ~iterator() = default;
-
-        bool operator==(const iterator& other) { return ptr == other->ptr;}
-        bool operator!=(const iterator& other) {return !(*this == other);}
-        reference operator*() { return ptr->data; }
-        pointer operator->() { return &(ptr->data); }
-        
-        iterator& operator++() { ptr = ptr->next; return *this;}
-        iterator operator++(int) {iterator old{*this}; ptr = ptr->next; return old;}
-
-
-        //iterator(node* ptr) : ptr_(ptr) {} // really needed?
-
-
-    private:
-        node* ptr{nullptr};
-
-    }
-
-    class const_iterator{
-        private:
-            node* ptr{nullptr};
-    }
     
     node* head{nullptr};
-    size_type size{0};
+    size_type size_{0};
+
+    T remove_handler(node* remove_next);
+
+public:
+  
+    LongList() = default;
+    LongList(const LongList&);
+    LongList(const LongList&&);
+    LongList& operator=(const LongList&);
+    LongList& operator=(const LongList&&);
+    ~LongList();
+    void insert(T);
+    T remove(size_type index);
+    bool operator==(const LongList& other) const { return std::equal(this->cbegin(), this->cend(), other->cbegin(), other->cend()); }
+    bool operator!=(const LongList& other) const { return !(*this==other); }
+    size_type size() const { return this->size_; }
+    size_type max_size() const { return 10000; } 
+    void swap(LongList& rhs){ std::swap(this->head, rhs->head); std::swap(this->size_, rhs->size_); }
+
+    template <typename ITERATOR_T>
+    class base_iterator{
+        // https://en.cppreference.com/w/cpp/named_req/ForwardIterator
+    public:
+        using pointer = ITERATOR_T*;
+        using reference = ITERATOR_T&;
+        using value_type = ITERATOR_T;
+
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::forward_iterator_tag;
+
+
+        base_iterator() = default;
+        base_iterator(const base_iterator& other) = default;
+        base_iterator& operator=(const base_iterator& other) = default;
+        ~base_iterator() = default;
+
+
+        bool operator==(const base_iterator& other) const { return ptr == other.ptr; }
+        bool operator!=(const base_iterator& other) const { return !(*this == other); }
+        reference operator*() const { return ptr->data; }
+        pointer operator->() const { return &(ptr->data); }
+        
+        base_iterator& operator++() { ptr = ptr->next; return *this;}
+        base_iterator operator++(int) { iterator old{*this}; ptr = ptr->next; return old;}
+    
+    private:
+        friend class LongList;
+        base_iterator(node* ptr) : ptr{ptr} {}
+        node* ptr{nullptr};
+    };
+
+    using iterator = base_iterator<value_type>;
+    using const_iterator = base_iterator<const value_type>;
+
+    iterator begin(){ iterator it{head}; return it; }
+    iterator end(){ iterator it{nullptr}; return it; }
+    const_iterator cbegin(){ const_iterator it{head}; return it; }
+    const_iterator cend(){ const_iterator it{nullptr}; return it; }
+    
+    T remove(iterator& pos) { }
+
+private:
+    
 
     void PrintErrorMessage(DWORD errorCode) {
         LPVOID lpMsgBuf;
@@ -101,7 +115,23 @@ private:
 };
 
 template<typename T>
-LongList<T>::LongList(const LongList& other) : size{other.size}{
+T LongList<T>::remove_handler(node* remove_next){
+    node* to_remove = remove_next->next;
+    remove_next->next = to_remove->next;
+    T data = to_remove->data;
+    VirtualFree(to_remove, 0, MEM_RELEASE);
+    return data;
+}
+
+// TODO: IMPLEMENT DELETE FUNCTIONS
+
+
+
+template<typename T>
+void swap(const LongList<T>& lhs, const LongList<T>& rhs){ lhs.swap(rhs); }
+
+template<typename T>
+LongList<T>::LongList(const LongList& other) : size_{other.size_}{
 
 }
 template<typename T>
